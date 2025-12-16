a11ef0c8ef4d8b45d295f5a4a04cc1951382d50b
Luminos <doug1992kch@gmail.com>
2025-12-16 20:23:02 +0800

    Delete assets directory

--- assets/js/stripe.js
+++
@@ -1,9 +0,0 @@
-export async function getStripeLink() {
-  const cfg = await (await fetch("/assets/data/products.json", { cache: "no-store" })).json();
-  return cfg.stripe_payment_link;
-}
-
-export async function goCheckout() {
-  const url = await getStripeLink();
-  window.location.href = url;
-}
