6d2e46144c8ab6544248eb885e8952ffe66e7ebd
Luminos <doug1992kch@gmail.com>
2025-12-16 20:23:31 +0800

    Delete engine directory

--- engine/pro-access.js
+++
@@ -1,9 +0,0 @@
-
-const PRO_KEY = "lumi_pro_unlocked_until";
-function isProUnlocked() {
-  const until = localStorage.getItem(PRO_KEY);
-  return until && Date.now() < Number(until);
-}
-function unlockPro(days = 7) {
-  localStorage.setItem(PRO_KEY, Date.now() + days*86400000);
-}
